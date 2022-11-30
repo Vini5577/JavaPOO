1515c5f075297f0547e312bf5ce3530e3c2d23be
Hugo Vinícius <ugopessoa@gmail.com>
2022-11-30 11:42:22 -0300

    finalizando o segundo exercicio de heranca

--- oo/heranca/Monstro.java
+++ oo/heranca/Monstro.java
@@ -2,19 +2,12 @@ package oo.heranca;
 
 public class Monstro extends Jogador {
     
-    boolean atacar(Jogador oponente){
-        
-        int deltaX = Math.abs(x - oponente.x);
-        int deltaY = Math.abs(y - oponente.y);
+    
+   Monstro(){
+     super(0, 0);
+   }
 
-        if(deltaX == 0 && deltaY == 1) {
-            oponente.vida -= 10;
-            return true;
-        } else if (deltaX == 1 && deltaY == 0) {
-            oponente.vida -= 10;
-            return true;
-        } else {
-            return false;
-        }
-    }
+   Monstro (int x, int y) {
+    super(x, y);
+   }
 }
--- oo/heranca/desafio/Carro.java
+++ oo/heranca/desafio/Carro.java
@@ -2,10 +2,20 @@ package oo.heranca.desafio;
 
 public class Carro {
     
+    final int VELOCIDADE_MAXIMA;
     int velocidadeAtual;
+    int delta = 5;
+
+    Carro(int velocidade_maxima) {
+        VELOCIDADE_MAXIMA = velocidade_maxima;
+    }
 
     void acelerar() {
-        velocidadeAtual +=5;
+        if(velocidadeAtual + delta > VELOCIDADE_MAXIMA){
+            velocidadeAtual = VELOCIDADE_MAXIMA;
+        } else {
+            velocidadeAtual += delta;
+        }
     }
 
     void frear() {
