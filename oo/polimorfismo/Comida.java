ed6b0ccadbdf693bb3eaded64803e24df48af57b
Hugo Vinícius <ugopessoa@gmail.com>
2022-12-05 08:54:18 -0300

    Desafio polimofismo

--- oo/polimorfismo/Comida.java
+++ oo/polimorfismo/Comida.java
@@ -1,26 +1,20 @@
 package oo.polimorfismo;
 
-public class Pessoa {
+public class Comida {
+    
     private double peso;
 
-    public Pessoa(double peso) {
+    public Comida(double peso) {
         setPeso(peso);
     }
 
-    public void comer(Comida comida) {
-        this.peso += comida.getPeso();
-    }
-    
     public double getPeso() {
         return peso;
     }
 
-    public void setPeso(double peso) {
-        
+    public void setPeso(double peso) { 
         if(peso >= 0) {
             this.peso = peso;
-
         }
     }
-    
 }
